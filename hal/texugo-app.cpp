1603d8b09114d007c01e4b18de38954d5aaadc7f
Matheus Castello <matheus@castello.eng.br>
2018-06-29 00:16:10 -0400

    platform: main: add logic to listening kernel uevent
    Listening kernel uevent
    
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- hal/texugo-app.cpp
+++ hal/texugo-app.cpp
@@ -1,9 +1,25 @@
 #include <stdio.h>
 #include <FuelGauge.h>
+#include <UdevListener.h>
 
 int main(int argc, char *argv[])
 {
-	
+	/* declare */
+	char subsys[] = "power_supply";
+	char dev_name[] = "BAT1";
+	UdevListener* udev = new UdevListener();
+	FuelGauge* gauge = new FuelGauge();
+
+	/* initialize */
+	udev->setSubSystem(subsys);
+	gauge->setDeviceName(dev_name);
+
+	while(1) {
+		udev->startListening();
+		printf("UEVENT has been received from %s!\n", 
+			udev->getDeviceName());
+		printf("Stage of Charge: %d%\n", gauge->getStateOfCharge());
+	}
 
 	return 0;
 }
