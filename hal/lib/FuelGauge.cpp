514161999b8e24215fded0f07ba716d063a3a9b3
Matheus Castello <matheus@castello.eng.br>
2018-06-28 20:03:38 -0400

    hal: Add Fuel Gauge Abstraction
    Add fuel gauge code to get uevents
    
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

---
+++ hal/lib/FuelGauge.cpp
@@ -0,0 +1,27 @@
+#include <FuelGauge.h>
+
+FuelGauge::FuelGauge () 
+{
+	this->udev = udev_new(this->FUEL_GAUGE_SYSPATH);
+	if (!this->udev)
+		throw std::runtime_error("Cannot create udev context!");
+
+	/* get device */
+	this->dev = udev_device_new_from_syspath(udev, this->FUEL_GAUGE_SYSPATH);
+}
+
+FuelGauge::getStateOfCharge ()
+{
+	/* get capacity attr */
+	this->soc = atoi(udev_device_get_sysattr_value(this->dev, "capacity"));
+	return this->soc;
+}
+
+FuelGauge::getVoltageNow ()
+{
+	/* get voltage_now attr */
+	this->voltage = 
+		atoi(udev_device_get_sysattr_value(this->dev, "voltage_now"));
+
+	return this->voltage / 100f;
+}
---
+++ hal/lib/FuelGauge.h
@@ -0,0 +1,21 @@
+#ifndef __HEADER_FUELGAUGE__
+#define __HEADER_FUELGAUGE__
+
+#include <stdio.h>
+#include <libudev.h>
+#include <stdexcept>
+
+class FuelGauge {
+	int soc;
+	float voltage;
+	static const char[34] FUEL_GAUGE_SYSPATH = 
+		"/sys/class/power_supply/battery";
+	struct udev *udev;
+	struct udev_device *dev;
+public:
+	FuelGauge ();
+	int getStateOfCharge ();
+	int getVoltageNow ();
+};
+
+#endif
