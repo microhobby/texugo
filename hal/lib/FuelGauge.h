315637bc5a5be4ca2a9a85c94d5bbdde0f8467c4
Matheus Castello <matheus@castello.eng.br>
2018-06-28 20:21:47 -0400

    platform: hal: fix char array declaration
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- hal/lib/FuelGauge.h
+++ hal/lib/FuelGauge.h
@@ -8,7 +8,7 @@
 class FuelGauge {
 	int soc;
 	float voltage;
-	static const char[34] FUEL_GAUGE_SYSPATH = 
+	static const char FUEL_GAUGE_SYSPATH[] = 
 		"/sys/class/power_supply/battery";
 	struct udev *udev;
 	struct udev_device *dev;
