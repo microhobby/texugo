9050ee2f82d29d28f1618c54eaada9ff2e183131
Matheus Castello <matheus@castello.eng.br>
2018-07-09 15:58:30 -0400

    HAL: GPIO: Example of use new GPIO chardevice API
    This is only an example
    
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

---
+++ hal/gpio/gpio_example.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <linux/gpio.h>
+#include <sys/ioctl.h>
+
+int main (int argc, char *argv[]) 
+{
+	struct gpiohandle_request req;
+	struct gpiohandle_data data;
+	int fd, handle_fd, pin;
+
+	/* get pin argument */
+	pin = atoi(argv[1]);
+
+	/* open char device */
+	fd = open("/dev/gpiochip0", 0);
+	
+	/* build request */
+	req.lineoffsets[0] = pin;
+	req.lines = 1;
+	req.flags = GPIOHANDLE_REQUEST_OUTPUT;
+	strcpy(req.consumer_label, "test");
+
+	/* request by ioctl */
+	handle_fd = ioctl(fd, GPIO_GET_LINEHANDLE_IOCTL, &req);
+
+	/* blink forever */
+	while (1) {
+		data.values[0] = 1;
+		ret = ioctl(req.fd, GPIOHANDLE_SET_LINE_VALUES_IOCTL, &data);
+		sleep(1);
+		data.values[0] = 0;
+		ret = ioctl(req.fd, GPIOHANDLE_SET_LINE_VALUES_IOCTL, &data);
+	}
+
+	return 0;
+}
+
