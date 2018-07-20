40a1c6f1ccb64e7f05edd9408a9160929a795d10
Matheus Castello <matheus@castello.eng.br>
2018-07-19 23:54:42 -0400

    HAL: defconfig: Add condition check for redefine
    When build we can now change SERVER_IP as agument from
    Makefile using:
    
    make compile ARGS="-DDEBUG -DSERVER_IP=192.168.0.11"
    
    When we try to use it before this patch the main defconfig
    will redefine the SERVER_IP and the argument is lost, now we
    check if SERVER_IP is already defined and not redefine it.
    
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- hal/lib/main_defconfig.h
+++ hal/lib/main_defconfig.h
@@ -3,7 +3,11 @@
 #define DEV_SOUND		"clap-sensor"
 #define PLATFORM_SUBSYS 	"platform"
 #define POWER_SUPPLY_SUBSYS 	"power_supply"
+
+/* can be redefined by makefile */
+#ifndef SERVER_IP
 #define SERVER_IP		"192.168.0.11"
+#endif
 
 /* int */
 #define VOLTAGE_PRECISION	10000
